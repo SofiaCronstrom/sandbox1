a20e212eb1f9eff5089778a61d0eb60b9f63d82e
sofiacronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2023-09-21 13:01:22 +0000

    Create box.js and useFollowPointer.js

---
+++ src/components/useFollowPointer.js
@@ -0,0 +1,25 @@
+import { useState, useRef, useEffect } from "react";
+
+export function useFollowPointer() {
+  const [point, setPoint] = useState({ x: 0, y: 0 });
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handlePointerMove = ({ clientX, clientY }) => {
+      if (ref.current) {
+        const element = ref.current;
+
+        const x = clientX - element.offsetLeft - element.offsetWidth / 2;
+        const y = clientY - element.offsetTop - element.offsetHeight / 2;
+        setPoint({ x, y });
+      }
+    };
+    window.addEventListener("pointermove", handlePointerMove);
+
+    return () => {
+      window.removeEventListener("pointermove", handlePointerMOve);
+    };
+  }, []);
+
+  return { ref, point };
+}
